5ef0b4ce7fcabd0256527941b35cc664ad8971ac
Soren Mathiasen <soren@schantz.com>
2013-01-19 20:48:41 +0100

    made a better page flow

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import play.api.mvc._
-import play.api.data.validation.Constraints._
 import projection.{LeagueProjection, ProfileProjection, MatchInfoProjection}
 import com.scalaprog.engine.Server
 import commands.{RegisterMatchScore, CreateUserProfile}
@@ -9,18 +8,10 @@ import models._
 import play.api.data._
 import play.api.data.Forms._
 import java.util.UUID
-import scala.collection.JavaConversions._
 
 
 object Application extends Controller {
 
-  val profileForm = Form(
-    mapping(
-      "profileName" -> nonEmptyText.verifying("Username not unique" , user => isUserNameUnique(user)) ,
-      "email" -> nonEmptyText.verifying("Not a valid email address", e => e.contains("@") && e.contains(".")),
-      "password" -> nonEmptyText
-    )(User.apply)(User.unapply)
-  )
 
   val registerNewMatchForm = Form(
     tuple(
@@ -31,12 +22,6 @@ object Application extends Controller {
     )
   )
 
-  def isUserNameUnique(name: String) : Boolean = {
-    println("profile projections name")
-    println(ProfileProjection.names.mkString("\n"))
-    println("unique name "+(!ProfileProjection.names.contains(name)))
-    !ProfileProjection.names.contains(name.trim)
-  }
 
   def index = Action {
     // Ok("index")
@@ -48,30 +33,11 @@ object Application extends Controller {
     println(ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList)
     println("leagues...")
     println(LeagueProjection.leagues.map(l => (l._1.toString, l._2)).toSeq)
-    Ok(views.html.index("", MatchInfoProjection.getScores.toList, ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList, LeagueProjection.leagues.toList, profileForm, registerNewMatchForm))
+    Ok(views.html.index("", MatchInfoProjection.getScores.toList, ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList, LeagueProjection.leagues.toList, registerNewMatchForm))
     //Ok(views.html.index("Your new application is ready.", null)  )
   }
 
 
-
-
-
-  def registerProfile = Action { implicit request =>
-    println("registred profile called")
-
-    profileForm.bindFromRequest.fold(
-      formWithErrors => {// binding failure, you retrieve the form containing errors,
-        println("-----------------")
-        println(formWithErrors)
-        println("-----------------")
-        BadRequest(views.html.index("", MatchInfoProjection.getScores.toList, ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList, LeagueProjection.leagues.toList,formWithErrors, registerNewMatchForm))
-      },
-      value => {// binding success, you get the actual value
-        Server.execute(CreateUserProfile(UUID.randomUUID(), value.profileName, value.password, value.email))
-        Ok(views.html.index("", MatchInfoProjection.getScores.toList, ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList, LeagueProjection.leagues.toList, profileForm, registerNewMatchForm))
-      }
-    )
-  }
 
   def addNewMatch = Action { implicit request =>
     var newData = Map[String, String]()
@@ -82,7 +48,7 @@ object Application extends Controller {
         println("-------SCORE FORM ----------")
         println(formWithErrors)
         println("-----------------")
-        BadRequest(views.html.index("", MatchInfoProjection.getScores.toList, ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList, LeagueProjection.leagues.toList, profileForm, formWithErrors))
+        BadRequest(views.html.index("", MatchInfoProjection.getScores.toList, ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList, LeagueProjection.leagues.toList,  formWithErrors))
       },
       value => {// binding success, you get the actual value
         println("yay it worked")
@@ -96,7 +62,7 @@ object Application extends Controller {
 
 
         Server.execute(RegisterMatchScore(league, teamOne, teamTwo, scoreOne, scoreTwo))
-        Ok(views.html.index("", MatchInfoProjection.getScores.toList, ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList,LeagueProjection.leagues.toList, profileForm, registerNewMatchForm))
+        Ok(views.html.index("", MatchInfoProjection.getScores.toList, ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList,LeagueProjection.leagues.toList,  registerNewMatchForm))
       }
     )
   }
@@ -105,4 +71,5 @@ object Application extends Controller {
   def createNewLeague = Action {
     Ok(views.html.createNewLeague(null))
   }
+
 }
---
+++ app/controllers/UserController.scala
@@ -0,0 +1,77 @@
+package controllers
+
+
+import play.api.mvc._
+import projection.ProfileProjection
+import com.scalaprog.engine.Server
+import commands.CreateUserProfile
+import models._
+import play.api.data._
+import play.api.data.Forms._
+import java.util.UUID
+
+object UserController extends Controller {
+
+  val loginForm = Form(
+    tuple(
+      "username" -> nonEmptyText,
+      "password" -> nonEmptyText
+    )
+  )
+
+  val profileForm = Form(
+    mapping(
+      "profileName" -> nonEmptyText.verifying("Username not unique" , user => isUserNameUnique(user)) ,
+      "email" -> nonEmptyText.verifying("Not a valid email address", e => e.contains("@") && e.contains(".")),
+      "password" -> nonEmptyText
+    )(User.apply)(User.unapply)
+  )
+
+  def isUserNameUnique(name: String) : Boolean = {
+    println("profile projections name")
+    println(ProfileProjection.names.mkString("\n"))
+    println("unique name "+(!ProfileProjection.names.contains(name)))
+    !ProfileProjection.names.contains(name.trim)
+  }
+
+  def login = Action { implicit request =>
+    println("registred profile called")
+
+    loginForm.bindFromRequest.fold(
+      formWithErrors => {// binding failure, you retrieve the form containing errors,
+        println("-----------------")
+        println(formWithErrors)
+        println("-----------------")
+        BadRequest(views.html.login(loginForm))
+      },
+      value => {// binding success, you get the actual value
+        if(!ProfileProjection.names.contains(value._1))
+          BadRequest(views.html.login(loginForm))
+        //Server.execute(CreateUserProfile(UUID.randomUUID(), value.profileName, value.password, value.email))
+        Redirect(routes.Application.index())
+        //Ok(views.html.index("", MatchInfoProjection.getScores.toList, ProfileProjection.namesAndUUIDS.map(d => (d._1, d._2)).toList, LeagueProjection.leagues.toList,  null)
+      }
+    )
+  }
+
+
+
+  def registerProfile = Action { implicit request =>
+    println("registred profile called")
+    profileForm.bindFromRequest.fold(
+      formWithErrors => {// binding failure, you retrieve the form containing errors,
+        println("-----------------")
+        println(formWithErrors)
+        println("-----------------")
+        BadRequest(views.html.registerProfile(formWithErrors))
+      },
+      value => {// binding success, you get the actual value
+        println("saving user")
+        Server.execute(CreateUserProfile(UUID.randomUUID(), value.profileName, value.password, value.email))
+        Redirect(routes.UserController.login())
+        //Ok(views.html.index())
+      }
+    )
+  }
+
+}
